afcc6fc378707c8b7f3a48148ea0786baeda3640
Marcelo Santiago <110051919+gabs-ki@users.noreply.github.com>
2023-10-24 10:24:12 -0300

    Update NaoEncontrado.jsx

--- tcc/src/pages/naoEncontrado/NaoEncontrado.jsx
+++ tcc/src/pages/naoEncontrado/NaoEncontrado.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function naoEncontrado() {
+function NaoEncontrado() {
   return (
     <div>naoEncontrado</div>
   )
 }
 
-export default naoEncontrado
+export default NaoEncontrado
